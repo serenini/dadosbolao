8343f283923946187c16f1bea28a984c4cf363d6
Renan <32426180+serenini@users.noreply.github.com>
2022-11-24 07:33:52 -0600

    Update New_test.py

--- New_test.py
+++ New_test.py
@@ -4,10 +4,10 @@
 # In[2]:
 
 
+import seaborn as sns
 import streamlit as st
 import pandas as pd
 import numpy as np
-import seaborn as sns
 import matplotlib.pyplot as plt
 import warnings
 warnings.filterwarnings('ignore')
