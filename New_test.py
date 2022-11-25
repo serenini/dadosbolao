9a6cca5767ff2b1f7892144177cadbc171206f5e
Renan <32426180+serenini@users.noreply.github.com>
2022-11-25 14:03:48 -0600

    Add files via upload

---
+++ New_test.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import seaborn as sns
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings('ignore')
+####### Load Dataset #####################
+bets=pd.read_csv('bets.csv', index_col='id')
+matches=pd.read_csv('matches.csv', nrows=48)
+completo=pd.read_csv('users_completo.csv')
+ligas=pd.read_json("http://vaiteqatar.online:8000/bet_groups")
+leagues={}
+for i in range(len(ligas)):
+    leagues[ligas['name'][i]]=pd.json_normalize(ligas['players'][i])
+##### Manipulate datasets
+
+bets['score']=bets['HomeTeamScore'].astype('str')+"x"+bets['AwayTeamScore'].astype('str')
+matches['match']=matches['MatchNumber'].astype(str)+"-"+matches['HomeTeam']+ " x "+matches['AwayTeam']
+bets=pd.merge(bets, completo, left_on='punter_username',right_on='username', how='left')
+bets=bets[['id','game_number', 'HomeTeamScore', 'AwayTeamScore', 'punter_username',
+        'name','score' ]]
+bets.rename(columns={"name": "Nome", "score": "Placar"}, inplace=True)
+
+########################################################
+st.set_page_config(layout="wide")
+
+st.markdown("## Palpites da galera")   ## Main Title
+
+################# Scatter Chart Logic #################
+
+st.sidebar.markdown("### Selecione a pelada desejada")
+
+
+Jogo = st.sidebar.selectbox("Jogo", matches['match'].to_list())
+game_selected=matches[matches['match']==Jogo]['MatchNumber'].tolist()
+bets_selected=bets[bets['game_number']==game_selected[0]]
+st.sidebar.markdown("### Filtre o placar pra dar aquela secada")
+Placar_selecionado = st.sidebar.selectbox("Placar", ["TODOS"]+bets_selected.sort_values('Placar')['Placar'].unique().tolist())
+st.sidebar.markdown("### Filtrar por liga")
+liga_selecionada = st.sidebar.selectbox("Liga", list(ligas['name']))
+
+#################################################################
+bar_fig=plt.figure(figsize=(8,4))
+ax=sns.countplot(x=bets_selected['Placar'], color='red',edgecolor='blue', order=bets_selected['Placar'].value_counts().index)
+ax.bar_label(ax.containers[0])
+plt.xlabel('Placar')
+plt.ylabel('Número de apostas')
+
+##################### Layout Application ##################
+
+container1 = st.container()
+with container1:
+    bar_fig
+    st.markdown('## Quem tá cravando?')
+    
+    
+
+
+# In[25]:
+
+
+bets_selected=bets_selected[bets_selected['id'].isin(leagues[liga_selecionada]['id'])]
+if Placar_selecionado=="TODOS":
+    bets_selected_2=bets_selected
+else:
+    bets_selected_2=bets_selected[bets_selected['Placar']==Placar_selecionado]
+bets_selected_2[['Nome','Placar']]
+
+
+# In[ ]:
+
+
+#traduzir jogos filtrar por liga
+
