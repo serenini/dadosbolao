70e7c3fec44be8dbc8a69ef1e030f90e8e6bf6fd
Renan <32426180+serenini@users.noreply.github.com>
2022-11-29 12:25:08 -0600

    Update New_test.py

--- New_test.py
+++ New_test.py
@@ -4,26 +4,40 @@
 # In[1]:
 
 
-import seaborn as sns
-import streamlit as st
 import pandas as pd
 import numpy as np
+import ipywidgets as widgets
+import seaborn as sns
 import matplotlib.pyplot as plt
 import warnings
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from ipywidgets import interact, interactive, fixed, interact_manual
 warnings.filterwarnings('ignore')
-####### Load Dataset #####################
-bets=pd.read_csv('bets.csv', index_col='id')
+#########FUNCTION TO GET DATA FROM GOOGLE SHEETS API################
+def get_game_scores_df():
+    scopes = [
+    'https://www.googleapis.com/auth/spreadsheets',
+    'https://www.googleapis.com/auth/drive'
+    ]
+    credentials = ServiceAccountCredentials.from_json_keyfile_name("GoogleSheetsAPIKey.json", scopes)
+    file = gspread.authorize(credentials)
+    sheet = file.open("GameScores")
+    sheet = sheet.worksheet("game_scores")
+    return pd.DataFrame(sheet.get_all_records())
+
+##################### Load Dataset #####################
+bets=pd.read_csv('https://raw.githubusercontent.com/serenini/dadosbolao/main/bets.csv', index_col='id')
 matches=pd.read_csv('matches.csv', nrows=48)
-completo=pd.read_csv('users_completo.csv')
+completo=pd.read_json("http://vaiteqatar.online:8000/users")
+scores=get_game_scores_df()
 ligas=pd.read_json("http://vaiteqatar.online:8000/bet_groups")
 leagues={}
 for i in range(len(ligas)):
     leagues[ligas['name'][i]]=pd.json_normalize(ligas['players'][i])
-##### Manipulate datasets
-bets=pd.read_csv('https://raw.githubusercontent.com/serenini/dadosbolao/main/bets.csv', index_col='id')
-scores=pd.read_csv('game_scores.csv')
-matches=pd.read_csv('matches.csv', nrows=48)
-completo=pd.read_json("http://vaiteqatar.online:8000/users")
+
+################## Manipulate datasets######################
+
 scores.rename(columns={'HomeTeamScore':'HomeTeamResult','AwayTeamScore':'AwayTeamResult'}, inplace=True)
 bets['Aposta']=bets['HomeTeamScore'].astype('str')+"x"+bets['AwayTeamScore'].astype('str')
 matches['match']=matches['MatchNumber'].astype(str)+"-"+matches['HomeTeam']+ " x "+matches['AwayTeam']
@@ -33,6 +47,7 @@ bets=bets[['id','game_number', 'HomeTeamScore', 'AwayTeamScore', 'punter_usernam
 bets.rename(columns={"name": "Nome"}, inplace=True)
 bets=pd.merge(bets,scores, left_on='game_number', right_on='MatchNumber', how='left')
 bets.drop('MatchNumber', axis=1, inplace=True)
+bets.replace('', np.nan, inplace=True)
 bets2=bets[bets['HomeTeamResult'].notna()]
 bets2['Resultado']=bets2['HomeTeamResult'].astype(int).astype('str')+"x"+bets2['AwayTeamResult'].astype(int).astype('str')
 bets2['Jogo']=bets2['HomeTeam']+" x "+bets2['AwayTeam']
@@ -53,7 +68,6 @@ def HIGHLIGHT(row):
     yellow = 'background-color: yellow;'
     green = 'background-color: green;'
 
-
     if row['Pontuação'] == "3":
         return [green]
     elif row['Pontuação'] == "1":
@@ -71,17 +85,23 @@ tab_grafico, tab_individual= st.tabs(["Análise por jogo", "Seus Palpites"])
 
 with tab_grafico:
     
-    Jogo = st.selectbox("Selecione a pelada desejada", matches['match'].to_list())
-    game_selected=matches[matches['match']==Jogo]['MatchNumber'].tolist()
+    Jogo = st.selectbox("Selecione a pelada desejada", "0"+matches['match'].to_list())
+    if Jogo=="0":
+        game_selected=matches[matches['match']=="1-Qatar x Ecuador"]['MatchNumber'].tolist()
+    else:
+        game_selected=matches[matches['match']==Jogo]['MatchNumber'].tolist()    
     bets_selected=bets[bets['game_number']==game_selected[0]]
     #################################################################
-    bar_fig=plt.figure(figsize=(8,4))
-    ax=sns.countplot(x=bets_selected['Aposta'], color='red',edgecolor='blue', order=bets_selected['Aposta'].value_counts().index)
-    ax.bar_label(ax.containers[0])
-    plt.xlabel('Aposta')
-    plt.ylabel('Número de apostas')
-    plt.title (Jogo)
-    bar_fig
+    if Jogo=="0":
+        st.markdown("")
+    else:
+        bar_fig=plt.figure(figsize=(8,4))
+        ax=sns.countplot(x=bets_selected['Aposta'], color='red',edgecolor='blue', order=bets_selected['Aposta'].value_counts().index)
+        ax.bar_label(ax.containers[0])
+        plt.xlabel('Aposta')
+        plt.ylabel('Número de apostas')
+        plt.title (Jogo)
+        bar_fig
     #################################################################
     st.markdown("#### Filtre o Aposta pra vem quem tá cravando")
     Aposta_selecionado = st.selectbox("Aposta", ["TODOS"]+bets_selected.sort_values('Aposta')['Aposta'].unique().tolist())
